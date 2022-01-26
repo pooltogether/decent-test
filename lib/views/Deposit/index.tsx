c0ae2fa38d7b03b01e00778a47315a47d6b2becd
Charles Bergeron <chuck.bergeron@gmail.com>
2022-01-26 11:48:12 -0800

    feat: aslk

--- lib/views/Deposit/index.tsx
+++ lib/views/Deposit/index.tsx
@@ -9,6 +9,7 @@ export const DepositUI = () => {
   return (
     <PagePadding className='flex flex-col space-y-6'>
       <UpcomingPrizeCard className='mt-4' />
+      aslkdnakjfnsajkn
       <DepositCard />
     </PagePadding>
   )
